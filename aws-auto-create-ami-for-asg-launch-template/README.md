bb817d5dd998d60b7461a707449db3ed86642998
abaid778 <abaid.gulshan@gmail.com>
2023-01-06 05:45:54 +0500

    init Python script for AUTO AMI update

---
+++ aws-auto-create-ami-for-asg-launch-template/README.md
@@ -0,0 +1,21 @@
+## Automating the creation of AMIs , create new version of Launch Template and the update of an Auto Scaling group 
+
+The code is used for a CI/CD pipeline which updates an auto scaling group of EC2 instances and creates an AMI of this instances. Than a new launch template version is created with this AMI.
+
+
+The [lambdacode.py](/lambdacode.py) is the code used by the Lambda function to create a new AMI and update the launch configuration.
+To use it on a Lambda function you will need to set the timeout to 5 seconds and input the enviroment variables:
+
+|Name|Stands For|
+|----|-----|
+|Auto_Scaling_Group_Name|Name of the Auto Scaling group that is going to be updated|
+|EC2_Role_ARN|Instance Profile ARN of the IAM Role for the EC2|
+|EC2_Security_Group_ID|Security Group's ID for the EC2 instances|
+|Launch_Template_Name|Name of the Launch Tamplate that is used by the Auto Scailing group|
+|Private_Key_Name|Name of the Private Key used to acess your EC2 instances|
+
+
+## License
+
+This library is licensed under the MIT-0 License. See the LICENSE file.
+
---
+++ aws-auto-create-ami-for-asg-launch-template/lambdacode.py
@@ -0,0 +1,68 @@
+import os
+import boto3
+import json
+import datetime
+
+start_date = str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+autoScalingGroupName = os.environ['Auto_Scaling_Group_Name']
+launchTemplateName = os.environ['Launch_Template_Name']
+ec2RoleARN = os.environ['EC2_Role_ARN']
+keyName = os.environ['Private_Key_Name']
+ec2SecurityGroupID = os.environ['EC2_Security_Group_ID']
+
+def lambda_handler(event, context):
+    
+
+        client = boto3.client('autoscaling')
+        autoscaling = client.describe_auto_scaling_groups(
+            AutoScalingGroupNames=[
+            autoScalingGroupName
+            ]
+        )
+
+        instanceID = autoscaling['AutoScalingGroups'][0]['Instances'][0]['InstanceId']
+        client = boto3.client('ec2')
+        name= "Lambda for " + autoScalingGroupName + " from "+ start_date
+        description= "AMI for " + autoScalingGroupName + " created by lambda"
+
+        image = client.create_image(
+            Description=description,
+            DryRun=False, 
+            NoReboot=True,
+            InstanceId=instanceID, 
+            Name=name)
+
+        launch_template= client.create_launch_template_version(
+            DryRun=False,
+            LaunchTemplateName = launchTemplateName,
+            SourceVersion="$Latest",
+            VersionDescription = name,
+            LaunchTemplateData={
+                'IamInstanceProfile': {
+                    'Arn': ec2RoleARN
+                },
+                'ImageId': image['ImageId'],
+                'KeyName': keyName,
+                'SecurityGroupIds': [
+                    ec2SecurityGroupID,
+                ]
+            }
+        )
+        
+        response = client.modify_launch_template(
+            LaunchTemplateName=launchTemplateName,
+            DefaultVersion="$Latest"
+        )
+        print("Default launch template set to $Latest.")
+        previous_version = str(
+            int(response["LaunchTemplate"]["LatestVersionNumber"]) - 4)
+        response = client.delete_launch_template_versions(
+            LaunchTemplateName=launchTemplateName,
+            Versions=[
+                previous_version,
+            ]
+        )        
+        launch_template_status = launch_template
+        print(launch_template_status)
+        
+        return json.dumps(response)  
